6c404560f2a9a337738e08053f5c5626ae40633f
Ray <rayliu672@gmail.com>
2022-06-23 00:24:58 -0700

    Changes to Function Name

--- Controller/PIDControl.cpp
+++ Controller/PIDControl.cpp
@@ -1,68 +1,68 @@
 #include "PIDControl.h" 
 
-PidControl::PidControl(float kp, float ki, float kd)
+pidControl::pidControl(float kp, float ki, float kd)
 {
     _ki = ki;
     _kd = kd;
     _kp = kp;    
 }
 
-float PidControl::control(float ref, float y_out, float dt)
+float pidControl::control(float ref, float yout, float dt)
 {
     // Call error_calc fxn to calculate P_error and I_error
-    float error = PI_error(ref, y_out, dt);
+    float error = piError(ref, yout, dt);
     // Derivative Error
     float error_dot = (error - _prev_error) / dt;
     // Used to calculate rate at which error changes
     _prev_error = error;
-    return PID_error(error, error_dot);
+    return pidError(error, error_dot);
 }
 
-float PidControl::control(float ref, float ref_dot, float y_out, float yout_dot, float dt)
+float pidControl::control(float ref, float ref_dot, float yout, float yout_dot, float dt)
 {
     // Call error_calc fxn to calculate P_error and I_error
-    float error = PI_error(ref, y_out, dt);
+    float error = piError(ref, yout, dt);
     // Calculates error based on expected angular velocity vs sensor data
     float error_dot = ref_dot - yout_dot;
     // Calculates PID error
-    return PID_error(error, error_dot);
+    return pidError(error, error_dot);
 }
 
-float PidControl::PI_error(float ref, float y_out, float dt)
+float pidControl::piError(float ref, float yout, float dt)
 {
     // Proportional error
-    float error = ref - y_out;
+    float error = ref - yout;
     // Integral error
-    integral_error = integral_error + (error * dt);
+    _integral_error = _integral_error + (error * dt);
     return error;
 }
 
-float PidControl::PID_error(float error, float error_dot)
+float pidControl::pidError(float error, float error_dot)
 {
  // Calculates the PID error and returns it
- return _kp*error + _ki*integral_error + _kd*error_dot;   
+ return _kp*error + _ki*_integral_error + _kd*error_dot;   
 }
 
-float PdControl::control(float ref, float y_out, float dt)
+float PdControl::control(float ref, float yout, float dt)
 {
     // Proportional error
-    float error = ref - y_out;   
+    float error = ref - yout;   
     // Derivative Error
     float error_dot = (error - _prev_error) / dt;
     // Calculates Pd_error
-    return Pd_error(error, error_dot);
+    return pdError(error, error_dot);
 }
-float PdControl::control(float ref, float ref_dot, float y_out, float yout_dot)
+float PdControl::control(float ref, float ref_dot, float yout, float yout_dot)
 {
     // Proportional Error
-    float error = ref - y_out;
+    float error = ref - yout;
     // Calculates error based on expected angular velocity vs sensor data
     float error_dot = ref_dot - yout_dot;
     // Calculates PD error
-    return Pd_error(error, error_dot);
+    return pdError(error, error_dot);
 }
 
-float PdControl::Pd_error(float error, float error_dot)
+float PdControl::pdError(float error, float error_dot)
 {
     // Calculates the PID error and returns it
     return _kp*error + _kd*error_dot;  
--- Controller/PIDControl.h
+++ Controller/PIDControl.h
@@ -1,46 +1,46 @@
 #ifndef PIDCONTROL_H
 #define PIDCONTROL_H
 
-class PidControl
+class pidControl
 {
     // PID Functions
     public:
         // constructor creates object
-        PidControl(float kp, float ki, float kd);
+        pidControl(float kp, float ki, float kd);
        /**
          * @param ref reference angle 
-         * @param y_out angle sensor data
+         * @param yout angle sensor data
          * @param dt difference between current and previous time
          * used to calculate error in through numerical integration
          */  
 
-        float control(float ref, float y_out, float dt);
+        float control(float ref, float yout, float dt);
         /**
          * @param ref reference angle (position) 
-         * @param y_out angle sensor data output (position) 
+         * @param yout angle sensor data output (position) 
          * @param dt difference between current and previous time
          * Calculate error through numerical differentiation(sensor data)
          */ 
 
-        float control(float ref, float ref_dot, float y_out, float yout_dot, float dt);
+        float control(float ref, float ref_dot, float yout, float yout_dot, float dt);
         /**
          * @param ref reference angle (position) 
          * @param ref_dot reference rate (velocity) 
-         * @param y_out angle sensor data output (position) 
+         * @param yout angle sensor data output (position) 
          * @param yout_dot angular rate deriviatve from sensor data (velocity)
          * @param dt difference between current and previous time
          * Calculate error through numerical differentiation(sensor data)
          */ 
 
-        float PI_error(float ref, float y_out, float dt);
+        float piError(float ref, float yout, float dt);
         /**
          * @param ref reference angle 
-         * @param y_out angle sensor data output
+         * @param yout angle sensor data output
          * @param dt difference between current and previous time
          * Calculates the Proportional and Integral error in PID
          */
 
-        float PID_error(float error, float error_dot);
+        float pidError(float error, float error_dot);
         /**
          * @param error Proportional error  
          * @param error_dot Derivative error
@@ -52,7 +52,7 @@ class PidControl
         float _ki;
         float _kd;
         float _prev_error = 0.0f;
-        float integral_error = 0.0f;
+        float _integral_error = 0.0f;
 };
 
 class PdControl
@@ -62,25 +62,25 @@ class PdControl
         // constructor creates object
         PdControl(float kp, float kd);
 
-        float control(float ref, float y_out, float dt);
+        float control(float ref, float yout, float dt);
         /**
          * @param ref reference angle (position) 
-         * @param y_out angle sensor data (position) 
+         * @param yout angle sensor data (position) 
          * @param dt difference between current and previous time
          * Calculate error through numerical differentiation(sensor data)
          */ 
 
-        float control(float ref, float ref_dot, float y_out, float yout_dot);
+        float control(float ref, float ref_dot, float yout, float yout_dot);
         /**
          * @param ref reference angle (position) 
          * @param ref_dot reference rate (velocity) 
-         * @param y_out angle sensor data output (position) 
+         * @param yout angle sensor data output (position) 
          * @param yout_dot angular rate deriviatve from sensor data (velocity)
          * @param dt difference between current and previous time
          * Calculate error through numerical differentiation(sensor data)
          */ 
 
-        float Pd_error(float error, float error_dot);
+        float pdError(float error, float error_dot);
         /**
          * @param error Proportional error  
          * @param error_dot Derivative error
