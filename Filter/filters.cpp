6028651d26a5453e8ea253ebefa0fb79a75736c1
Ray <rayliu672@gmail.com>
2022-06-24 22:43:14 -0700

    Filter added

---
+++ Filter/filters.cpp
@@ -0,0 +1,48 @@
+#include "filters.h"
+
+LowpassFilter::LowpassFilter(float time_const)
+{
+    _time_const = time_const;
+}
+
+LowpassFilter::~LowpassFilter()
+{
+}
+float LowpassFilter::get_time_const()
+{
+    return _time_const;
+}
+
+void LowpassFilter::set_time_const(float curr_time)
+{
+    _time_const = curr_time;
+}
+float LowpassFilter::filter(float val, float dt)
+{
+    // insert low pass filter contents    
+    const float a = dt / (_time_const + dt);
+    float filtered_val = a * val + (1 - a) * _prev_filtered_val;
+    _prev_filtered_val = filtered_val; 
+
+    return filtered_val;
+}
+
+HighpassFilter::HighpassFilter(float time_const)
+{
+    _time_const = time_const;
+}
+
+HighpassFilter::~HighpassFilter()
+{
+}
+
+float HighpassFilter::filter(float val, float dt)
+{
+    // insert high pass filter contents
+    
+    const float a = _time_const / (_time_const + dt);
+    float filtered_val = a*_prev_filtered_val + a*(val-prev_val);
+    _prev_filtered_val = filtered_val;
+    prev_val = val;
+    return filtered_val;
+}
